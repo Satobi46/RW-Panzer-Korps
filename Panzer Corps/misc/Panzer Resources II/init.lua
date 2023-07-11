165f4214c389d03d1417db854d4a6d91181ada01
Satobi <83565801+Satobi46@users.noreply.github.com>
2023-07-11 12:03:11 +0800

    2.1a

---
+++ Panzer Corps/misc/Panzer Resources II/init.lua
@@ -0,0 +1,14 @@
+--[[
+=== PANZER KORPS KORPORATION ===
+]]--
+
+function PanzerDeathKorpsInitialized()
+	local z = ProceduralRandom(69,69)
+	print(">>>PANZER KORPS KORPORATION<<<")
+end
+
+PanzerKorps( "data" )
+PanzerKorps( "buildings" )
+PanzerKorps( "misc" )
+PanzerKorps( "infantry, vehicles" )
+PanzerKorps( "music" )
